829133e54bab49b60a5112e20a355df8ce221724
Kamil Bochno <kamilas12345@gmail.com>
2021-04-29 18:36:00 +0200

    Add not registered link

--- loginform.php
+++ loginform.php
@@ -8,20 +8,23 @@
     <link href="css/bootstrap.css" rel="stylesheet">
     
 </head>
-<body style="background-image: url(images/background.jpg); background-repeat: no-repeat; background-size: cover; max-width: 1550px; height: 100%;">
+<body style="background-image: url(images/background.jpg); background-repeat: no-repeat; background-size: cover; max-width: 1550px; height: 500px;">
 
     
     <div class="container d-flex justify-content-center text-center" style="margin-top: 200px; background-color: #D1D1D1; max-width:400px;">
-            <form>
+            <form action="login.php" method="post">
                 <p class="fs-3 text-center">Log in</p>
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="email" name="email"class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="col text-center">
+                  <p class="fs-5">Not registered? <a href="registerform.php">Click here</a></p>
                 </div>
                 <div class="col text-center" style="margin-bottom: 10px;">
                 <button type="submit" class="btn btn-primary">Submit</button>
