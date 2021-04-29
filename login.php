019987ec0f6c12c06049ebcbf25d67470e1bbe9e
Kamil Bochno <kamilas12345@gmail.com>
2021-04-29 22:15:53 +0200

    Connect to database

--- login.php
+++ login.php
@@ -1,9 +1,44 @@
 <?php
 
-  $email = $_POST['email'];
-  $password = $_POST['password'];
+  require_once "connect.php";
+  
+  $connect = @new mysqli($host,$db_user,$db_password, $db_name);
+  
+  if ($connect->connect_errno!=0)
+  {
+      echo "Error: ".$connect->connect_errno;
+  }
 
-  echo $email. "<br />";
-  echo $password;
+  else
+  {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'"; 
+
+    if ($result = @$connect->query($sql))
+    {
+        $how_many_users = $result->num_rows;
+        if($how_many_users>0)
+        {
+          $char = $result->fetch_assoc();
+          $login = $char['email'];
+
+          
+
+          $result->close();
+
+          header('Location: index.php');
+        }  else {
+
+        }
+    }
+
+
+    $connect->close();
+  }
+  
+
+  
 
 ?>
