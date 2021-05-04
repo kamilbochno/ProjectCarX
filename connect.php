e89f5fa5f358785383a61b6043b350fd9719b084
Kamil Bochno <kamilas12345@gmail.com>
2021-05-04 09:00:09 +0200

    Connect db with registration

--- connect.php
+++ connect.php
@@ -1,8 +1,7 @@
 <?php
-  $host= "localhost";
   $db_user = "root";
-  $db_password = "";
+  $db_pass = "";
   $db_name = "users";
 
-
-?>
+  $db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
