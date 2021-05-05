bf54fbf96716a077318f4cc7a82ac35bb664427a
Kamil Bochno <kamilas12345@gmail.com>
2021-05-05 22:42:32 +0200

    Add logged session

---
+++ index_logged.php
@@ -0,0 +1,227 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+
+     <!-- Links to the libraries needed -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+
+   
+     <!-- Initialize the slider -->
+     <script>
+       $(document).ready(function(){
+         $('.slider-area').bxSlider({
+            auto: true,
+            pager: true,
+            slideWidth: 600,
+            responsive: true
+            
+
+         });
+       });
+     </script>
+
+     <script>
+      // Initialize and add the map
+      function initMap() {
+        // The location of Uluru
+        const cracow = { lat: 50.04, lng: 19.56 };
+        // The map, centered at Uluru
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 4,
+          center: cracow,
+        });
+        // The marker, positioned at Uluru
+        const marker = new google.maps.Marker({
+          position: cracow,
+          map: map,
+        });
+      }
+    </script>
+
+    <title>ProjectCarX</title>
+  </head>
+  <body>
+  
+  <main>
+    
+  
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      
+        <a class="navbar-brand" href="#">
+          <img src="images/Component 1.jpg" alt="carX_brand">
+          CarX
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active px-2" aria-current="page" href="index_logged.php">Home</a>
+            </li>
+            <li class="nav-item px-2">
+              <a class="nav-link" href="index_logged.php#services_description">What we do?</a>
+            </li>
+            <li class="nav-item px-2">
+              <a class="nav-link" href="index_logged.php#contact">Contact</a>
+            </li>
+          </div>
+          <h5 style="color:aliceblue" class="nav-item px-2">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h5>
+    <p>
+        
+          
+    
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul class="navbar-nav">
+              <li class="nav-item px-2">
+              <a class="nav-link" href="logout.php">Sign Out</a>
+              <li class="nav-item px-2">
+                <a class="nav-link" href="loginform.php">Log In</a>
+              </li>
+            
+            
+              <li class="nav-item px-2">
+                <a class="nav-link" href="registerform.php">Register</a>
+              </li>
+            </ul>
+      
+      </div>
+        
+      
+        </ul>
+        </div>
+      </div>
+    </nav>
+  
+
+
+    <!-- Slider HTML --> 
+      <div class="container pt-5 d-flex justify-content-center">
+        <div class="row">
+          <div class="slider-area">
+            <div>
+              <img src="images/slide1_1.jpg" alt="slider_image" class="img-fluid">
+            </div>
+            
+            <div>
+              <img src="images/slide2_2.jpg" alt="slider_image" class="img-fluid">
+            </div>
+            <div>
+              <img src="images/slide3_3.jpg" alt="slider_image" class="img-fluid">
+            </div>
+          
+        </div>
+      </div>
+      </div>
+
+      <div class="container d-flex flex-column" id="services_description">
+        <div class="card-group">
+           
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-5">COMPLETE SERVICES</h5>
+              <img src="images/gears.svg" class="card-img-top pt-5 img-fluid h-25" alt="gears">
+              <p class="card-text pt-5">Cubilipende sollicitudineed leo pharea aumennec in velit veaugun e diam lor innia est. Proin dictum elem velit Fterdpibus sceleue vitaepe.</p>
+              <div class="container pt-3 d-flex justify-content-center">
+                <button type="button" class="btn btn-primary btn-lg ">MORE</button>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-5">OIL CHANGE</h5>
+              <img src="images/Oil.svg" class="card-img-top pt-5 img-fluid h-25" alt="oil">
+              <p class="card-text pt-5">Cubilipende sollicitudineed leo pharea aumennec in velit veaugun e diam lor innia est. Proin dictum elem velit Fterdpibus sceleue vitaepe.</p>
+              <div class="container pt-3 d-flex justify-content-center">
+                <button type="button" class="btn btn-primary btn-lg ">MORE</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-5">ENGINE REPAIR</h5>
+              <img src="images/engine.svg" class="card-img-top pt-5 img-fluid h-25" alt="engine">
+              <p class="card-text pt-5">Cubilipende sollicitudineed leo pharea aumennec in velit veaugun e diam lor innia est. Proin dictum elem velit Fterdpibus sceleue vitaepe.</p>
+              <div class="container pt-3 d-flex justify-content-center">
+                <button type="button" class="btn btn-primary btn-lg ">MORE</button>
+              </div>
+            
+            </div>
+          </div>
+        </div>
+    
+        <div class="container pt-5">
+          <div class="card" id="contact">
+            <div class="card-body">
+              <div class="container">
+                <div class="row">
+                  
+                  <div class="col-sm">
+                    
+                    <h5 class="card-title pt-3">CONTACT US</h5>
+              <p class="card-text">
+                Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within a matter of hours to help you.
+              </p>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Your message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="9"></textarea>
+                
+              </div>
+              
+                  </div>
+                  
+                  <div class="col-sm pt-5">
+                    <!--The div element for the map -->
+                     <div id="map"></div>
+
+                  </div>
+              
+            </div>
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+
+  </div>
+  </div>
+  
+  </main>
+  
+    <script src="js/bootstrap.js"></script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMhXgb1EjTImNKDGYquracXaSJxRff0jY&callback=initMap&libraries=&v=weekly"
+      async
+    ></script>
+  </body>
+</html>
